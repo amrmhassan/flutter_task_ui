b4dadb0a9cc876584a79d881341ee3b4e0b267dd
Amr Hassan <amrhassanpersonal@gmail.com>
2023-11-04 20:29:57 +0200

    efficeint api calls

--- lib/data/datasources/users_datasource.dart
+++ lib/data/datasources/users_datasource.dart
@@ -2,13 +2,17 @@ import 'package:dio/dio.dart';
 import 'package:ui_task/constants/constants.dart';
 import 'package:ui_task/data/models/user_model.dart';
 
+List<UserModel> _usersRepo = [];
+
 class UsersDatasource {
   //? this will load all users from the api in the form of dart models
   Future<List<UserModel>> getUsers() async {
+    if (_usersRepo.isNotEmpty) return _usersRepo;
     Dio dio = Dio();
     var res = await dio.get(Constants.apiLink);
     var body = res.data as List;
     var models = body.map((e) => UserModel.fromJson(e)).toList();
+    _usersRepo = models;
     return models;
   }
 }
